e74257b8356926ae477be2a076c1aa18d292d3be
YeongCC <D190147B@sc.edu.my>
2021-05-22 15:55:33 +0800

    Create Activity_helper.java

---
+++ RobotControl/app/src/main/java/handsomec/robotcontrol/Activity_helper.java
@@ -0,0 +1,21 @@
+package handsomec.robotcontrol;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.preference.PreferenceManager;
+
+public class Activity_helper {
+    public static void initialize(Activity activity) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+
+        String orientation = prefs.getString("prefOrientation", "Null");
+        if ("Landscape".equals(orientation)) {
+            activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        } else if ("Portrait".equals(orientation)) {
+            activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        } else {
+            activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
+        }
+    }
+}
